d97b0a97eaa5404bd36eb754b19812f3bb3b5696
monseg73 <monsegcarretero@gmail.com>
2022-01-16 12:05:05 +0100

    Creación de la clase vector

--- prueba1/src/prueba1/Vector.java
+++ prueba1/src/prueba1/Vector.java
@@ -13,6 +13,7 @@ public class Vector {
 			System.out.printf("Elemento %d=%d\n",i,a[i], args);
 		for (int i=0;i<5;i++)
 			resultado +=a[i];
+		//visualizamos la media de los valores calculados
 		System.out.println("La media es = " + resultado/5);
 		
 	}
