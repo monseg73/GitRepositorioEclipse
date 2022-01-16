b2e70ce55945000bdc3eb0f4ac353f34d2867f88
monseg73 <61168303+monseg73@users.noreply.github.com>
2022-01-16 12:26:41 +0100

    Modificación de la variable n

--- prueba1/src/prueba1/Vector.java
+++ prueba1/src/prueba1/Vector.java
@@ -3,18 +3,18 @@ package prueba1;
 public class Vector {
 
 	public static void main(String[] args) {
-		// TODO Esbozo de mÈtodo generado autom·ticamente
+		// TODO Esbozo de m√©todo generado autom√°ticamente
 		int n=5, resultado=0;
-		int a[]= new int[5];
-		for (int i=0; i<5;i++)
+		int a[]= new int[n];
+		for (int i=0; i<n;i++)
 			a[i]=(i+1) * 10;
 		// visualizamos el vector 
-		for (int i=0;i<5;i++)
+		for (int i=0;i<n;i++)
 			System.out.printf("Elemento %d=%d\n",i,a[i], args);
-		for (int i=0;i<5;i++)
+		for (int i=0;i<n;i++)
 			resultado +=a[i];
 		//visualizamos la media de los valores calculados
-		System.out.println("La media es = " + resultado/5);
+		System.out.println("La media es = " + resultado/n);
 		
 	}
 
