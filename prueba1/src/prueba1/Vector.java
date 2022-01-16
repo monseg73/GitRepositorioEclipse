c84a2a9ab93f011f6d57265f3dea82670f7e47a3
monseg73 <monsegcarretero@gmail.com>
2022-01-16 11:43:59 +0100

    Subiendo creación de vector

---
+++ prueba1/src/prueba1/Vector.java
@@ -0,0 +1,20 @@
+package prueba1;
+
+public class Vector {
+
+	public static void main(String[] args) {
+		// TODO Esbozo de método generado automáticamente
+		int n=5, resultado=0;
+		int a[]= new int[5];
+		for (int i=0; i<5;i++)
+			a[i]=(i+1) * 10;
+		// visualizamos el vector 
+		for (int i=0;i<5;i++)
+			System.out.printf("Elemento %d=%d\n",i,a[i], args);
+		for (int i=0;i<5;i++)
+			resultado +=a[i];
+		System.out.println("La media es = " + resultado/5);
+		
+	}
+
+}
